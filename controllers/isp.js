230909a64c0eb87fa1cdc6e2be8711346a15e00c
Milind Sharma <milind.sharma@searce.com>
2020-02-22 11:02:32 +0530

    isp get and post changes

--- controllers/isp.js
+++ controllers/isp.js
@@ -1,14 +1,42 @@
 const { ErrorHandler } = require('../utils/errorHandler');
-const get_isp_list = (req, res, next) => {
-    res.send({
-        name: 'flower service',
-        imageUrl: req.protocol + '://' + req.get('host') + '/images/flower.jpg'
-    });
+const mongoose = require('mongoose');
+const { ISPModel, validateAddISP } = require('../models/isp');
+const _ = require('lodash');
+
+const setDataForClient = (req, data) => {
+    let newData = _.cloneDeep(data);
+    newData.image = `${req.protocol}://${req.get('host')}/images/${newData.image}`;
+    delete newData.__V;
+    return newData;
 }
-const add_isp = (req, res, next) => {
-    console.log('file', req.file);
-    console.log('body', req.body);
-    res.send({ file: req.file })
+
+const get_isp_list = async (req, res, next) => {
+    try {
+        let fetchedISPLIst = await ISPModel.find({}).sort('name');
+        fetchedISPLIst.map((data) => { return setDataForClient(req, data) });
+        res.send({ 'ispList': fetchedISPLIst });
+    }
+    catch (error) {
+        next(new ErrorHandler(500, error.message));
+    }
+}
+const add_isp = async (req, res, next) => {
+    if (!req.file) return next(new ErrorHandler(400, 'No image added'));
+    let ispData = req.body;
+    ispData.image = req.file.filename;
+
+    const { error } = validateAddISP(ispData);
+    if (error) return next(new ErrorHandler(400, error.details[0].message));
+
+    ispData._id = new mongoose.Schema.Types.ObjectId();
+    let newISP = new ISPModel(ispData);
+    try {
+        let savedData = await newISP.save();
+        res.send(setDataForClient(savedData));
+    }
+    catch (error) {
+        next(new ErrorHandler(500, error.message));
+    }
 }
 module.exports.get_isp_list = get_isp_list;
 module.exports.add_isp = add_isp;
--- models/isp.js
+++ models/isp.js
@@ -1,22 +1,37 @@
 const mongoose = require('mongoose');
-
+const Joi = require('joi');
 const contactNumRegex = new RegExp("^[0-9() -]*$");
 const emailRegex = new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$");
 
 const ISPSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId(),
+    _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true, maxlength: 250 },
-    lowest_price: { type: Number, required: true },
-    rating: { type: Number, required: true, min: 0, max: 5, validate: /^\d$/, default: 0 },
+    lowest_price: { type: Number, required: true, index: true },
+    rating: { type: Number, required: true, min: 0, max: 5, validate: /^\d$/, default: 0, index: true },
     max_speed: { type: String, required: true, maxlength: 50 },
     description: { type: String, maxlength: 1500 },
     contact_number: { type: String, required: true, maxlength: 15, validate: contactNumRegex },
     email: { type: String, required: true, validate: emailRegex },
-    image: { type: String, maxlength: 1000 },
-    url: { type: String, maxlength: 1000 }
+    image: { type: String, maxlength: 1000, required: true },
+    url: { type: String, maxlength: 1000, required: true }
 })
 
-const ISPModel = mongoose.model('isp', ispSchema, 'isp');
+const ISPModel = mongoose.model('isp', ISPSchema, 'isp');
 
-module.exports.ISPSchema = ISPSchema;
+const validateAddISP = function (data) {
+    const schema = Joi.object({
+        name: Joi.string().required().max(10),
+        lowest_price: Joi.number().required(),
+        rating: Joi.number().max(5).min(0),
+        max_speed: Joi.string().max(50).required(true),
+        description: Joi.string().max(1500),
+        contact_number: Joi.string().required().max(15).regex(contactNumRegex),
+        email: Joi.string().required(true).regex(emailRegex),
+        image: Joi.string().max(1000).required(true),
+        url: Joi.string().max(1000).required(true)
+    })
+    return schema.validate(data);
+}
+
 module.exports.ISPModel = ISPModel;
+module.exports.validateAddISP = validateAddISP;
